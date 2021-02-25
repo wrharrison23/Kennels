090a00ecc6023b942feb02a81dbb7b746f427fb6
Wes <wrharrison23@gmail.com>
2021-02-25 13:20:26 -0500

    initial structure

---
+++ src/components/Employee/EmployeeCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Employee.css"
+export const EmployeeCard = () => (
+  <section className="employee">
+    <h3 className="employee__name">Emma Beaton</h3>
+    <div className="employee__address">Nashville Kennels North</div>
+  </section>
+);
